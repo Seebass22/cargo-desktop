99c68e5856121993a9724d08de87abee90e3a076
Sebastian Thuemmel <sebastianthuemmel@yahoo.ca>
2023-05-02 17:02:54 +0200

    print desktop file path

--- src/main.rs
+++ src/main.rs
@@ -15,7 +15,9 @@ fn main() {
     let cargo_toml = std::fs::read_to_string("Cargo.toml").unwrap();
     let cargo_toml: CargoToml = toml::from_str(&cargo_toml).unwrap();
 
-    let mut exec_path = home::home_dir().unwrap();
+    let home_dir = home::home_dir().unwrap();
+
+    let mut exec_path = home_dir.clone();
     exec_path.push(".cargo");
     exec_path.push("bin");
     exec_path.push(&cargo_toml.package.name);
@@ -33,5 +35,13 @@ comment=
         cargo_toml.package.name,
         exec_path.display(),
     );
+
+    let mut desktop_file_path = home_dir.clone();
+    desktop_file_path.push(".local");
+    desktop_file_path.push("share");
+    desktop_file_path.push("applications");
+    desktop_file_path.push(format!("{}.desktop", cargo_toml.package.name));
+
     println!("{}", desktop_file);
+    println!("{}", desktop_file_path.display());
 }
