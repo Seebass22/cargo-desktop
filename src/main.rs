608a5d46179ef08828e2135ae1b1fb792c9c31a5
Sebastian Thuemmel <sebastianthuemmel@yahoo.ca>
2023-05-02 16:41:18 +0200

    print desktop file

--- src/main.rs
+++ src/main.rs
@@ -8,11 +8,30 @@ struct CargoToml {
 #[derive(Deserialize, Debug)]
 struct Package {
     name: String,
-    description: Option<String>,
+    _description: Option<String>,
 }
 
 fn main() {
     let cargo_toml = std::fs::read_to_string("Cargo.toml").unwrap();
     let cargo_toml: CargoToml = toml::from_str(&cargo_toml).unwrap();
-    println!("{:?}", cargo_toml);
+
+    let mut exec_path = home::home_dir().unwrap();
+    exec_path.push(".cargo");
+    exec_path.push("bin");
+    exec_path.push(&cargo_toml.package.name);
+
+    let desktop_file = format!(
+        "[Desktop Entry]
+Encoding=UTF-8
+Type=Application
+NoDisplay=false
+Terminal=false
+Name={}
+Exec={}
+comment=
+",
+        cargo_toml.package.name,
+        exec_path.display(),
+    );
+    println!("{}", desktop_file);
 }
