c805aec39dcedfc1a4915ee10f2859fa92eb8601
Sebastian Thuemmel <sebastianthuemmel@yahoo.ca>
2023-05-04 20:01:53 +0200

    fix panic if $EDITOR has args

--- src/edit.rs
+++ src/edit.rs
@@ -2,8 +2,10 @@ use std::{env, path::Path, process::Command};
 
 pub fn edit(path: &Path) {
     let editor = env::var("EDITOR").unwrap_or("vi".into());
+    let editor = shell_words::split(&editor).expect("failed to parse $EDITOR");
 
-    Command::new(editor)
+    Command::new(&editor[0])
+        .args(&editor[1..])
         .arg(path)
         .status()
         .expect("Something went wrong");
